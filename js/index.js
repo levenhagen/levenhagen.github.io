031f293d4c37926e3ffac15b529dde9c6211f27b
Bernardo <levenhal@gmail.com>
2018-12-25 09:32:14 -0200

    last brushstrokes...

--- js/index.js
+++ js/index.js
@@ -1,18 +1,48 @@
-var sidebar_instance = null;
+var sidenav_instance = null;
 
 $(document).ready(function() {
   //initializing
   //$(".tap-target").tapTarget();
 
   //pulse each 3s
-  var sidenav = document.querySelectorAll(".sidenav");
-  sidebar_instance = M.Sidenav.init(sidenav, []);
+  var elems = document.querySelectorAll(".sidenav");
+  var sidenav_instances = M.Sidenav.init(elems, []);
+  sidenav_instance = M.Sidenav.getInstance(elems[0]);
+
   setInterval(showPulse, 6000);
 
-  $("#quote").height($("#photo").height());
+  var qh = $("#quote").height();
+  var ph = $("#photo").height();
+
+  if (qh > ph) {
+    $("#photo").height(qh);
+  } else {
+    $("#quote").height(ph);
+  }
 });
 
 //-==============- CUSTOM FUNCTIONS -==============-
+
+function home() {
+  $("#about").css("display", "none");
+  $("#portfolio").css("display", "none");
+  $("#home").css("display", "block");
+  sidenav_instance.close();
+}
+
+function about() {
+  $("#home").css("display", "none");
+  $("#portfolio").css("display", "none");
+  $("#about").css("display", "block");
+  sidenav_instance.close();
+}
+
+function portfolio() {
+  $("#home").css("display", "none");
+  $("#about").css("display", "none");
+  $("#portfolio").css("display", "block");
+  sidenav_instance.close();
+}
 
 function showPulse() {
   $(".nav-button").addClass("pulse");
