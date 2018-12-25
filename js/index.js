347549d92274d70e5f7b5a5aefb8a78eda315216
Bernardo <levenhal@gmail.com>
2018-12-25 05:49:30 -0200

    getting lit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 var sidebar_instance = null;
+var navbutton_instance = null;
 
 $(document).ready(function() {
   $(".tap-target").tapTarget();
 
-  var elems = document.querySelectorAll(".sidenav");
-  sidebar_instance = M.Sidenav.init(elems, []);
+  var sidenav = document.querySelectorAll(".sidenav");
+  sidebar_instance = M.Sidenav.init(sidenav, []);
 
-  window.setTimeout(openTapTarget, 13500);
+  setInterval(showPulse, 6000);
+  //window.setTimeout(openTapTarget, 13500);
   //window.setTimeout(closeTapTarget, 21000);
 });
+
+function showPulse() {
+  $(".nav-button").addClass("pulse");
+  setTimeout(removePulse, 3000);
+}
+
+function removePulse() {
+  $(".nav-button").removeClass("pulse");
+}
 
 function openTapTarget() {
   if (sidebar_instance[0].isOpen === false) {
